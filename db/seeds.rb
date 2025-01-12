f730ddf589c18d8b7c42150d37ce23b972d4d444
Mads Sobering <madisonsobering@gmail.com>
2025-01-12 15:25:35 -0600

    Updated the seed file to populate the database with the csv file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,26 @@ require "csv"
 #                   price: Faker::Commerce.price,
 #                   stock_quantity: Faker::Number.number(digits: 2))
 # end
+
+# Reset the database when re-running the seed file.
+Product.destroy_all
+Category.destroy_all
+
+# Parse and read the csv file into a variable.
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+# Make the csv data readable by Ruby.
+products = CSV.parse(csv_data, headers: true)
+
+# Loop through the data to create new records.
+products.each do |product|
+  new_category = Category.find_or_create_by(name: product["category"])
+
+  new_product = new_category.products.build(title: product["name"],
+                                            description: product["description"],
+                                            price: product["price"],
+                                            stock_quantity: product["stock quantity"])
+
+  new_product.save!
+end
