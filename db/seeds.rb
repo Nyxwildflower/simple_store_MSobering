1615dfe08568017b8114a0a5e9300fde1dc09fa4
Mads Sobering <madisonsobering@gmail.com>
2025-01-12 15:21:45 -0600

    Created and filled a csv file, and added the require line for it to the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "csv"
 
-# Create Fake products for the store
-676.times do
-  Product.create!(title: Faker::Commerce.product_name,
-                  price: Faker::Commerce.price,
-                  stock_quantity: Faker::Number.number(digits: 2))
-end
+# Q9: Create Fake products for the store
+# 676.times do
+#   Product.create!(title: Faker::Commerce.product_name,
+#                   price: Faker::Commerce.price,
+#                   stock_quantity: Faker::Number.number(digits: 2))
+# end
